097e5dd5415baea67d4adbdc2a7f311df335080c
Richard Rog <rrog@vcc.ca>
2021-04-27 16:48:05 -0700

    First Version 001

---
+++ list.py
@@ -0,0 +1,15 @@
+# initialize counter
+
+cou = 1
+
+# Iterate the loop 5 times
+
+while counter < 6:
+
+    # Print the counter value
+
+	print ("The current counter value: %d" %cou)
+
+    # Increment the counter
+
+	coun = coun + 1
